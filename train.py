45f839324aea06ffd9395b34f530f5c6163300e9
zhangliang <zhang_liang_work@163.com>
2022-05-03 23:03:52 +0800

    original

--- train.py
+++ train.py
@@ -87,118 +87,119 @@ def parse(args=None):
 
     return parser.parse_args(args)
 
-args = parse()
-print(args)
+if __name__ == '__main__':
+    args = parse()
+    print(args)
 
-args.lr_base = args.lr
-args.n_attrs = len(args.attrs)
-args.betas = (args.beta1, args.beta2)
+    args.lr_base = args.lr
+    args.n_attrs = len(args.attrs)
+    args.betas = (args.beta1, args.beta2)
 
-os.makedirs(join(args.data_save_root, args.experiment_name), exist_ok=True)
-os.makedirs(join(args.data_save_root, args.experiment_name, 'checkpoint'), exist_ok=True)
-os.makedirs(join(args.data_save_root, args.experiment_name, 'sample_training'), exist_ok=True)
-with open(join(args.data_save_root, args.experiment_name, 'setting.txt'), 'w') as f:
-    f.write(json.dumps(vars(args), indent=4, separators=(',', ':')))
+    os.makedirs(join(args.data_save_root, args.experiment_name), exist_ok=True)
+    os.makedirs(join(args.data_save_root, args.experiment_name, 'checkpoint'), exist_ok=True)
+    os.makedirs(join(args.data_save_root, args.experiment_name, 'sample_training'), exist_ok=True)
+    with open(join(args.data_save_root, args.experiment_name, 'setting.txt'), 'w') as f:
+        f.write(json.dumps(vars(args), indent=4, separators=(',', ':')))
 
-if args.data == 'CelebA':
-    from data import CelebA
-    train_dataset = CelebA(args.data_path, args.attr_path, args.img_size, 'train', args.attrs)
-    valid_dataset = CelebA(args.data_path, args.attr_path, args.img_size, 'valid', args.attrs)
-if args.data == 'CelebA-HQ':
-    from data import CelebA_HQ
-    train_dataset = CelebA_HQ(args.data_path, args.attr_path, args.image_list_path, args.img_size, 'train', args.attrs)
-    valid_dataset = CelebA_HQ(args.data_path, args.attr_path, args.image_list_path, args.img_size, 'valid', args.attrs)
-train_dataloader = data.DataLoader(
-    train_dataset, batch_size=args.batch_size, num_workers=args.num_workers,
-    shuffle=True, drop_last=True
-)
-valid_dataloader = data.DataLoader(
-    valid_dataset, batch_size=args.n_samples, num_workers=args.num_workers,
-    shuffle=False, drop_last=False
-)
-print('Training images:', len(train_dataset), '/', 'Validating images:', len(valid_dataset))
+    if args.data == 'CelebA':
+        from data import CelebA
+        train_dataset = CelebA(args.data_path, args.attr_path, args.img_size, 'train', args.attrs)
+        valid_dataset = CelebA(args.data_path, args.attr_path, args.img_size, 'valid', args.attrs)
+    if args.data == 'CelebA-HQ':
+        from data import CelebA_HQ
+        train_dataset = CelebA_HQ(args.data_path, args.attr_path, args.image_list_path, args.img_size, 'train', args.attrs)
+        valid_dataset = CelebA_HQ(args.data_path, args.attr_path, args.image_list_path, args.img_size, 'valid', args.attrs)
+    train_dataloader = data.DataLoader(
+        train_dataset, batch_size=args.batch_size, num_workers=args.num_workers,
+        shuffle=True, drop_last=True
+    )
+    valid_dataloader = data.DataLoader(
+        valid_dataset, batch_size=args.n_samples, num_workers=args.num_workers,
+        shuffle=False, drop_last=False
+    )
+    print('Training images:', len(train_dataset), '/', 'Validating images:', len(valid_dataset))
 
-cafegan = CAFEGAN(args)
+    cafegan = CAFEGAN(args)
 
-if args.is_resume:
-    cafegan.load(os.path.join(args.data_save_root, args.experiment_name, 'checkpoint','weights.'+str(args.load_epoch)+'.pth'))
-    ea = event_accumulator.EventAccumulator(join(args.data_save_root, args.experiment_name, 'summary', args.event_name))
-    ea.Reload()
-    it = ea.scalars.Items('D/d_loss')[-1][1]
-else:
-    it = 0
-cudnn.benchmark = True
+    if args.is_resume:
+        cafegan.load(os.path.join(args.data_save_root, args.experiment_name, 'checkpoint','weights.'+str(args.load_epoch)+'.pth'))
+        ea = event_accumulator.EventAccumulator(join(args.data_save_root, args.experiment_name, 'summary', args.event_name))
+        ea.Reload()
+        it = ea.scalars.Items('D/d_loss')[-1][1]
+    else:
+        it = 0
+    cudnn.benchmark = True
 
-progressbar = Progressbar()
+    progressbar = Progressbar()
 
-writer = SummaryWriter(join(args.data_save_root, args.experiment_name, 'summary'))
+    writer = SummaryWriter(join(args.data_save_root, args.experiment_name, 'summary'))
 
-fixed_img_real, fixed_label_org = next(iter(valid_dataloader))
-fixed_img_real = fixed_img_real.cuda() if args.gpu else fixed_img_real
-fixed_label_org = fixed_label_org.cuda() if args.gpu else fixed_label_org
-fixed_label_org = fixed_label_org.type(torch.float)
-sample_attr_list = [fixed_label_org]
-for i in range(args.n_attrs):
-    tmp = fixed_label_org.clone()
-    tmp[:, i] = 1 - tmp[:, i]
-    tmp = check_attribute_conflict(tmp, args.attrs[i], args.attrs)
-    sample_attr_list.append(tmp)
+    fixed_img_real, fixed_label_org = next(iter(valid_dataloader))
+    fixed_img_real = fixed_img_real.cuda() if args.gpu else fixed_img_real
+    fixed_label_org = fixed_label_org.cuda() if args.gpu else fixed_label_org
+    fixed_label_org = fixed_label_org.type(torch.float)
+    sample_attr_list = [fixed_label_org]
+    for i in range(args.n_attrs):
+        tmp = fixed_label_org.clone()
+        tmp[:, i] = 1 - tmp[:, i]
+        tmp = check_attribute_conflict(tmp, args.attrs[i], args.attrs)
+        sample_attr_list.append(tmp)
 
-it_per_epoch = len(train_dataset) // args.batch_size
-for epoch in range(args.load_epoch, args.epochs):
-    # train with base lr in the first 100 epochs
-    # and half the lr in the last 100 epochs
-    lr = args.lr_base / 1.0 if epoch <100 else args.lr_base / 2.0
-    cafegan.set_lr(lr)
+    it_per_epoch = len(train_dataset) // args.batch_size
+    for epoch in range(args.load_epoch, args.epochs):
+        # train with base lr in the first 100 epochs
+        # and half the lr in the last 100 epochs
+        lr = args.lr_base / 1.0 if epoch <100 else args.lr_base / 2.0
+        cafegan.set_lr(lr)
 
-    writer.add_scalar('LR/learning_rate', lr, it+1)
-    for img_real, label_org in progressbar(train_dataloader):
-        cafegan.train()
+        writer.add_scalar('LR/learning_rate', lr, it+1)
+        for img_real, label_org in progressbar(train_dataloader):
+            cafegan.train()
 
-        img_real = img_real.cuda() if args.gpu else img_real
-        label_org = label_org.cuda() if args.gpu else label_org
+            img_real = img_real.cuda() if args.gpu else img_real
+            label_org = label_org.cuda() if args.gpu else label_org
 
-        # generate target domain labels randomly
-        idx = torch.randperm(len(label_org))
-        label_trg = label_org[idx].contiguous()
+            # generate target domain labels randomly
+            idx = torch.randperm(len(label_org))
+            label_trg = label_org[idx].contiguous()
 
-        c_org = label_org.clone()
-        c_trg = label_trg.clone()
+            c_org = label_org.clone()
+            c_trg = label_trg.clone()
 
-        c_org = c_org.cuda() if args.gpu else c_org
-        c_trg = c_trg.cuda() if args.gpu else c_trg
-        attr_diff = (c_trg - c_org) * args.thres_int
+            c_org = c_org.cuda() if args.gpu else c_org
+            c_trg = c_trg.cuda() if args.gpu else c_trg
+            attr_diff = (c_trg - c_org) * args.thres_int
 
-        if (it+1) % (args.n_d+1) != 0:
-            errD = cafegan.trainD(img_real, label_org, attr_diff)
-            add_scalar_dict(writer, errD, it+1, 'D')
-        else:
-            errG = cafegan.trainG(img_real, label_trg, attr_diff)
-            add_scalar_dict(writer, errG, it+1, 'G')
-            if 'errD' in vars():
-                progressbar.say(epoch=epoch, iter=it+1, d_loss=errD['d_loss'], g_loss=errG['g_loss'])
+            if (it+1) % (args.n_d+1) != 0:
+                errD = cafegan.trainD(img_real, label_org, attr_diff)
+                add_scalar_dict(writer, errD, it+1, 'D')
             else:
-                pass
+                errG = cafegan.trainG(img_real, label_trg, attr_diff)
+                add_scalar_dict(writer, errG, it+1, 'G')
+                if 'errD' in vars():
+                    progressbar.say(epoch=epoch, iter=it+1, d_loss=errD['d_loss'], g_loss=errG['g_loss'])
+                else:
+                    pass
 
-        if (it+1) % args.save_interval == 0:
-            # To save storage space, I only checkpoint the weights of G.
-            # If you'd like to keep weights of G, D, optim_G, optim_D,
-            # please use save() instead of saveG().
-            cafegan.save(os.path.join(
-                args.data_save_root, args.experiment_name, 'checkpoint', 'weights.{:d}.pth'.format(epoch)
-            ))
+            if (it+1) % args.save_interval == 0:
+                # To save storage space, I only checkpoint the weights of G.
+                # If you'd like to keep weights of G, D, optim_G, optim_D,
+                # please use save() instead of saveG().
+                cafegan.save(os.path.join(
+                    args.data_save_root, args.experiment_name, 'checkpoint', 'weights.{:d}.pth'.format(epoch)
+                ))
 
-        if (it+1) % args.sample_interval == 0:
-            cafegan.eval()
-            with torch.no_grad():
-                samples = [fixed_img_real]
-                for i, att_trg in enumerate(sample_attr_list):
-                    attr_diff = (att_trg - sample_attr_list[0]) * args.thres_int
-                    samples.append(cafegan.G(fixed_img_real, attr_diff))
-                samples = torch.cat(samples, dim=3)
-                writer.add_image('sample', vutils.make_grid(samples, nrow=1, normalize=True, range=(-1., 1.)), it+1)
-                vutils.save_image(samples, os.path.join(
-                        args.data_save_root, args.experiment_name, 'sample_training',
-                        'Epoch_({:d})_({:d}of{:d}).jpg'.format(epoch, it%it_per_epoch+1, it_per_epoch)
-                    ), nrow=1, normalize=True, range=(-1., 1.))
-        it += 1
+            if (it+1) % args.sample_interval == 0:
+                cafegan.eval()
+                with torch.no_grad():
+                    samples = [fixed_img_real]
+                    for i, att_trg in enumerate(sample_attr_list):
+                        attr_diff = (att_trg - sample_attr_list[0]) * args.thres_int
+                        samples.append(cafegan.G(fixed_img_real, attr_diff))
+                    samples = torch.cat(samples, dim=3)
+                    writer.add_image('sample', vutils.make_grid(samples, nrow=1, normalize=True, range=(-1., 1.)), it+1)
+                    vutils.save_image(samples, os.path.join(
+                            args.data_save_root, args.experiment_name, 'sample_training',
+                            'Epoch_({:d})_({:d}of{:d}).jpg'.format(epoch, it%it_per_epoch+1, it_per_epoch)
+                        ), nrow=1, normalize=True, range=(-1., 1.))
+            it += 1
